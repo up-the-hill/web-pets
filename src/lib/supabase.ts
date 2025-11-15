15c95f756c58c32f328ce8677554aa8ff98fbd09
up-the-hill <aj.sth.2022@gmail.com>
2025-11-15 14:37:56 +1100

    moved login state logic to supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,24 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, type User } from '@supabase/supabase-js'
+import { writable } from 'svelte/store';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+// manage user state
+export const user = writable<User | null>(null);
+
+supabase.auth.getUser().then(({ data }) => {
+  user.set(data.user);
+});
+
+supabase.auth.onAuthStateChange((event, session) => {
+  if (event == 'SIGNED_IN' && session) {
+    user.set(session.user);
+  } else if (event == 'SIGNED_OUT') {
+    user.set(null);
+  }
+});
+
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,32 +1,11 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { supabase } from '$lib/supabase';
-	import type { User } from '@supabase/supabase-js';
-
-	let user: User | null;
-
-	onMount(() => {
-		supabase.auth.getSession().then(({ data: { session } }) => {
-			user = session?.user ?? null;
-		});
-
-		const {
-			data: { subscription: authListener }
-		} = supabase.auth.onAuthStateChange((_, session) => {
-			const currentUser = session?.user;
-			user = currentUser ?? null;
-		});
-
-		return () => {
-			authListener?.unsubscribe();
-		};
-	});
+	import { user } from '$lib/supabase';
 </script>
 
 <header>
 	<a href="/">web-pets</a>
 	<div>
-		{#if user}
+		{#if $user}
 			<a href="/logout">logout</a>
 		{:else}
 			<a href="/login">login</a>
